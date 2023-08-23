cf089e8f84d923a42f569f8c2f56e2fd2626b50e
Vivek Sharma <viveksupmanyu@gmail.com>
2023-08-23 18:23:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function getCurrentDateAndTime() {
+  const dateTime = new Date();
+  return dateTime.toLocaleString();
+}
+const dateDisplay = document.getElementById("datetime");
+
+dateDisplay.value = getCurrentDateAndTime();
