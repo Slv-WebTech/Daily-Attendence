31cc46bf8c0abb64663d59c33074fa2189910c57
Vivek Sharma <viveksupmanyu@gmail.com>
2023-09-03 19:50:27 +0530

    media query and new quotes

--- motivation.js
+++ motivation.js
@@ -1,33 +1,34 @@
 const quotes = [
-  "Sales is a journey, not a destination. Keep pushing forward, and success will follow.",
-
-  "In sales, every 'no' brings you closer to a 'yes.' Keep going!",
-
-  "Sales is the art of turning rejection into motivation.",
-
-  "The key to sales success is persistence with a dash of optimism.",
-
-  "Sales is about creating opportunities, not waiting for them.",
-
-  "Don't count the calls you've made, make every call count.",
-
-  "Sales is a marathon, not a sprint. Keep your hope alive and keep running.",
-
-  "Rejection is just a stepping stone to perfection in sales.",
-
-  "The best salespeople are the ones who never lose hope in the face of adversity.",
-
-  "Success in sales is not about never failing; it's about never giving up.",
-
-  "Sales is a game of resilience. Keep your chin up and your spirits higher.",
-
-  "Every 'no' is a 'not yet' in the world of sales.",
-
-  "In sales, every day is a new opportunity to make a sale and reignite your hope.",
-
-  "Believe in your product, believe in yourself, and never stop believing in the power of hope.",
-
-  "Sales is about enthusiasm, persistence, and never-ending hope.",
+  "In every setback lies the opportunity for a comeback.",
+  "Keep your dreams alive, and your determination stronger.",
+  "Success is born from resilience, not perfection.",
+  "The darkest hour has only sixty minutes.",
+  "Every challenge you face today is a stepping stone to a brighter tomorrow.",
+  "Embrace difficulties as stepping stones to success.",
+  "Your determination is the compass that guides you through the storm.",
+  "A setback is just a setup for a comeback.",
+  "The road to success is often under construction; keep moving forward.",
+  "Hope fuels the fire of perseverance. Keep it burning.",
+  "In every challenge lies an opportunity; keep your hope alive.",
+  "When you feel like giving up, remember why you started.",
+  "Hope is the fuel that keeps us moving forward, even in the toughest of times.",
+  "Stay strong, stay hopeful, and success will find its way to you.",
+  "Every setback is a setup for a comeback; never lose hope.",
+  "Hardships may bend you, but they can't break your spirit.",
+  "Embrace difficulties as stepping stones to your dreams; hope will guide you.",
+  "The darkest hour has only sixty minutes; hold onto hope, and the dawn will come.",
+  "Your determination is the key to unlocking the door of opportunity.",
+  "In the world of challenges, hope is your greatest asset.",
+  "Keep your head up and your heart full of hope; great things await.",
+  "Difficulties are temporary; your resilience is permanent.",
+  "Hope is the light that shines through the darkest clouds of adversity.",
+  "Every day is a new chance to persevere and achieve your goals.",
+  "Believe in yourself, keep pushing forward, and success will be yours.",
+  "The road to success is paved with determination and hope.",
+  "Stay positive, stay hopeful, and watch the miracles unfold.",
+  "Obstacles are opportunities in disguise; don't lose sight of your dreams.",
+  "Hope is the compass that leads us through the storms of life.",
+  "No matter how tough it gets, remember that brighter days are ahead.",
 ];
 
 function displayMotivationQuote() {
